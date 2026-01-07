d30ed21dc90625331b1fcaf8e207dd816ad26a20
임승민 <seungmin3238@gmail.com>
2026-01-07 23:02:09 +0900

    Not Found 오류 처리 및 페이지 표시

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFound() {
+  return (
+    <div id="error">
+      <h1>Not Found</h1>
+      <p>we couldn't find the requested news article.</p>
+    </div>
+  );
+}
--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,9 +1,12 @@
+import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsDetailPage({ params }) {
   const { slug } = params;
-
   const news = DUMMY_NEWS.find((news) => news.slug === slug);
+
+  if (!news) notFound();
+
   return (
     <article className="news-article">
       <header>
